cb979ba13fd6c46bf9cc2dc363f86bf7e379615f
Estiven Alvarez <Estivezalvarez1902@hotmail.com>
2025-08-25 19:18:18 -0500

    Se crean dos clases

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,3 +1,32 @@
 from datetime import datetime
 
-# TODO: Add code here
+
+class Note:
+    def __init__(self, text: str, page: int, date: datetime):
+        self.text: str = text
+        self.page: int = page
+        self.date: datetime = date
+
+
+
+    def __str__(self)   -> str:
+        pass
+
+
+class Book:
+
+    EXCELLENT: int = 3
+    GOOD: int = 2
+    BAD: int = 1
+    UNRATED: int = -1
+
+    def __init__(self, isbn: str, tittle: str,author: str, pages: int):
+        self.isbn: str = isbn
+        self.tittle: str = tittle
+        self.author: str = author
+        self.pages: int = pages
+
+
+    def rating
+
+
