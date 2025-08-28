0c23ebeebe351d093d0cc08e7b8f725b7ac6c5df
Estiven <EstivenAlvarez1902@hotmail.com>
2025-08-28 17:47:21 -0500

    class ReadingDiary

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -20,13 +20,67 @@ class Book:
     BAD: int = 1
     UNRATED: int = -1
 
-    def __init__(self, isbn: str, tittle: str,author: str, pages: int, rating: int ,notes: list):
+    def __init__(self, isbn: str, tittle: str,author: str, pages: int):
         self.isbn: str = isbn
         self.tittle: str = tittle
         self.author: str = author
         self.pages: int = pages
         self.rating: int = Book.UNRATED
-        self.notes: list = []
+        self.notes: list[Note] = []
+
+    def add_note(self,text: str, page:int, date: datetime)-> bool:
+        if page > self.pages:
+            return False
+
+        n_note = Note(text, page, date)
+        self.notes.append(n_note)
+        return True
+
+
+    def set_rating(self, rating: int) -> bool:
+        if rating not in (Book.EXCELLENT, Book.GOOD, Book.BAD):
+            return False
+        self.rating = rating
+        return True
+
+    def get_notes_of_page (self, page: int) -> list[Note]:
+        notes_of_page = []
+
+    def page_with_most_notes (self) -> int:
+        if not self.notes:
+            return -1
+
+
+    def __str__ (self) -> str:
+        rating_str = {
+            Book.EXCELLENT: "excellent",
+            Book.GOOD: "good",
+            Book.BAD: "bad",
+            Book.UNRATED: "unrated"
+        }[self.rating]
+
+        return (f"ISBN: {self.isbn}\n"
+                f"Title: {self.tittle}\n"
+                f"Author: {self.author}\n"
+                f"Pages: {self.pages}\n"
+                f"Rating: {rating_str}")
+
+
+
+class ReadingDiary:
+    def __init__(self):
+        self.books: dict[str, Book] = {}
+
+    def add_book(self, isbn: str, title: str, author: str, pages: int) -> bool:
+        if isbn not in self.books:
+            return false
+        self.books[isbn] = Book(isbn, title, author, pages)
+        return True
+
+    def search_by_isbn(self, isbn: str) -> Book
+
+
+
 
 
 
